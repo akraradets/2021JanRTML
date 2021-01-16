f522a6de2cbf2d4664a169304aa554f464f5ddfc
akraradets <akraradets@gmail.com>
2021-01-16 13:58:17 +0700

    add docker build

---
+++ docker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAME=akraradet-lab
+SSH_PORT=21413
+
+cd "`dirname $0`"
+cd image
+docker build . -t $NAME
+cd ..
+docker run -d -p ${SSH_PORT}:22 -v $HOME/RTML:/RTML --gpus all --name $NAME $NAME
