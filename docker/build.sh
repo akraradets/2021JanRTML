3a47783f3a3eda265a3ab5d15c16ea159cfeca3f
akraradets <akraradets@gmail.com>
2021-01-17 12:53:28 +0700

    update docker

--- docker/build.sh
+++ docker/build.sh
@@ -7,4 +7,5 @@ cd "`dirname $0`"
 cd image
 docker build . -t $NAME
 cd ..
-docker run -d -p ${SSH_PORT}:22 -v $HOME/RTML:/root --gpus all --name $NAME $NAME
+docker run -d -p ${SSH_PORT}:22 -v $HOME/RTML:/root/RTML --gpus all --name $NAME $NAME
+# docker run -d -p 21413:22 -v $HOME/RTML:/root/RTML --gpus all --name akraradet-lab akraradet-lab
