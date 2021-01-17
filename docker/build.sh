8668c664a0707d4242cf3d815b517d5cf746561c
akraradets <akraradets@gmail.com>
2021-01-17 16:40:11 +0700

    init report

--- docker/build.sh
+++ docker/build.sh
@@ -8,4 +8,6 @@ cd image
 docker build . -t $NAME
 cd ..
 docker run -d -p ${SSH_PORT}:22 -v $HOME/RTML:/root/RTML --gpus all --name $NAME $NAME
-# docker run -d -p 21413:22 -v $HOME/RTML:/root/RTML --gpus all --name akraradet-lab akraradet-lab
+# docker run -d -p 21413:22 --gpus all --name akraradet-lab akraradet-lab
+# docker run -p 21413:22 --gpus all akraradet-lab
+
