1ee6cd0911ac99fd0cf23816ee5a1a21880b701d
akrarads <akrarads@gmail.com>
2021-03-04 10:55:16 +0000

    training face

---
+++ lab7/DCGAN-face.py
@@ -0,0 +1,264 @@
+
+import matplotlib.pyplot as plt
+from logger import Logger
+import torch
+from torch import nn, optim
+from torchvision import transforms, datasets
+
+
+class DiscriminativeNet(torch.nn.Module):
+    
+    def __init__(self):
+        super(DiscriminativeNet, self).__init__()
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3, out_channels=128, kernel_size=4, 
+                stride=2, padding=1, bias=False
+            ),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=128, out_channels=256, kernel_size=4,
+                stride=2, padding=1, bias=False
+            ),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=256, out_channels=512, kernel_size=4,
+                stride=2, padding=1, bias=False
+            ),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=512, out_channels=1024, kernel_size=4,
+                stride=2, padding=1, bias=False
+            ),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        self.out = nn.Sequential(
+            nn.Linear(1024*4*4, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        # Convolutional layers
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        # Flatten and apply sigmoid
+        x = x.view(-1, 1024*4*4)
+        x = self.out(x)
+        return x
+
+class GenerativeNet(torch.nn.Module):
+    
+    def __init__(self):
+        super(GenerativeNet, self).__init__()
+        
+        self.linear = torch.nn.Linear(100, 1024*4*4)
+        
+        self.conv1 = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=1024, out_channels=512, kernel_size=4,
+                stride=2, padding=1, bias=False
+            ),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=512, out_channels=256, kernel_size=4,
+                stride=2, padding=1, bias=False
+            ),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.conv3 = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=256, out_channels=128, kernel_size=4,
+                stride=2, padding=1, bias=False
+            ),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        self.conv4 = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=128, out_channels=3, kernel_size=4,
+                stride=2, padding=1, bias=False
+            )
+        )
+        self.out = torch.nn.Tanh()
+
+    def forward(self, x):
+        # Project and reshape
+        x = self.linear(x)
+        x = x.view(x.shape[0], 1024, 4, 4)
+        # Convolutional layers
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        # Apply Tanh
+        return self.out(x)
+
+
+
+
+def real_data_target(size):
+    '''
+    Tensor containing ones, with shape = size
+    '''
+    data = torch.ones(size, 1)
+    if torch.cuda.is_available(): return data.to('cuda:1')
+    return data
+
+def fake_data_target(size):
+    '''
+    Tensor containing zeros, with shape = size
+    '''
+    data = torch.zeros(size, 1)
+    if torch.cuda.is_available(): return data.to('cuda:1')
+    return data
+
+def train_discriminator(optimizer, real_data, fake_data):
+    # Reset gradients
+    optimizer.zero_grad()
+    
+    # Propagate real data
+    prediction_real = discriminator(real_data)
+    error_real = loss(prediction_real, real_data_target(real_data.size(0)))
+    error_real.backward()
+
+    # Propagate fake data
+    prediction_fake = discriminator(fake_data)
+    error_fake = loss(prediction_fake, fake_data_target(real_data.size(0)))
+    error_fake.backward()
+    
+    # Take a step
+    optimizer.step()
+    
+    # Return error
+    return error_real + error_fake, prediction_real, prediction_fake
+
+
+def train_generator(optimizer, fake_data):
+    # Reset gradients
+    optimizer.zero_grad()
+
+    # Propagate the fake data through the discriminator and backpropagate.
+    # Note that since we want the generator to output something that gets
+    # the discriminator to output a 1, we use the real data target here.
+    prediction = discriminator(fake_data)
+    error = loss(prediction, real_data_target(prediction.size(0)))
+    error.backward()
+    
+    # Update weights with gradients
+    optimizer.step()
+    
+    # Return error
+    return error
+
+def noise(batchSize):
+    n = torch.randn(batchSize, 100)
+    if torch.cuda.is_available(): return n.to('cuda:1') 
+    return n
+
+
+
+
+
+DATA_FOLDER = './torch_data/DCGAN/CelebA'
+def face_data():
+    compose = transforms.Compose(
+        [
+            transforms.Resize(64),
+            transforms.ToTensor(),
+            transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+        ])
+    out_dir = '{}/dataset'.format(DATA_FOLDER)
+    return datasets.CelebA(root=out_dir, split='all', transform=compose, download=True)
+    
+data = face_data()
+data_loader = torch.utils.data.DataLoader(data, batch_size=100, shuffle=True)
+num_batches = len(data_loader)
+# print(num_batches)
+
+
+
+# Custom weight initialization
+
+def init_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1 or classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(0.00, 0.02)
+
+# Instantiate networks
+
+generator = GenerativeNet()
+generator.apply(init_weights)
+discriminator = DiscriminativeNet()
+discriminator.apply(init_weights)
+
+# Enable cuda if available
+device = 'cuda:1'
+# if torch.cuda.is_available():
+generator.to(device)
+discriminator.to(device)
+
+
+
+# Optimizers
+
+d_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+g_optimizer = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+# Loss function
+
+loss = nn.BCELoss()
+
+# Number of epochs of training
+num_epochs = 100
+
+num_test_samples = 16
+test_noise = noise(num_test_samples)
+
+logger = Logger(model_name='DCGAN', data_name='FACE')
+
+for epoch in range(num_epochs):
+    for n_batch, (real_data,_) in enumerate(data_loader):
+
+        # Train Discriminator
+        
+        # if torch.cuda.is_available(): 
+        real_data = real_data.to('cuda:1')
+        fake_data = generator( noise(real_data.size(0)) ).detach()
+        d_error, d_pred_real, d_pred_fake = train_discriminator(d_optimizer, real_data, fake_data)
+
+        # Train Generator
+        
+        fake_data = generator(noise(real_data.size(0)))
+        g_error = train_generator(g_optimizer, fake_data)
+
+        # Log error and display progress
+        logger.log(d_error, g_error, epoch, n_batch, num_batches)
+        if (n_batch) % 100 == 0:
+            # display.clear_output(True)
+            # Display Images
+            test_images = generator(test_noise).data.cpu()
+            logger.log_images(test_images, num_test_samples, epoch, n_batch, num_batches);
+            # Display status Logs
+            logger.display_status(
+                epoch, num_epochs, n_batch, num_batches,
+                d_error, g_error, d_pred_real, d_pred_fake
+            )
+
+        # Save model checkpoints
+        logger.save_models(generator, discriminator, epoch)
