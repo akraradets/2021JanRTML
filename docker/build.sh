e7a59ad6ec9173e2e5ed332b85102476cb3af052
akrarads <akrarads@gmail.com>
2021-04-09 20:43:12 +0000

    half lab12

--- docker/build.sh
+++ docker/build.sh
@@ -3,15 +3,15 @@
 NAME=akraradet-lab
 SSH_PORT=21413
 
-cd "`dirname $0`"
-cd image
-docker build . -t $NAME
-cd ..
+#cd "`dirname $0`"
+#cd image
+#docker build . -t $NAME
+#cd ..
 # docker run -d -p ${SSH_PORT}:22 -v $HOME/docker_root:/root/RTML --gpus all --name $NAME $NAME
 
-docker run -d -p ${SSH_PORT}:22 -v $HOME/docker_root:/root/RTML -v /home/fidji/mdailey/COCO:/root/COCO -v /home/fidji/mdailey/Cityscapes:/root/Cityscapes  --gpus all --name $NAME $NAME
+#docker run -d -p ${SSH_PORT}:22 -v $HOME/docker_root:/root/RTML -v /home/fidji/mdailey/COCO:/root/COCO -v /home/fidji/mdailey/Cityscapes:/root/Cityscapes  --gpus all --name $NAME $NAME
 
 # docker run -d -p 21413:22 -v $HOME/docker_root:/root/RTML --gpus all --name akraradet-lab akraradet-lab
 # docker run -p 21413:22 --gpus all akraradet-lab
 
-# docker run -d -p 21413:22 -v $HOME/docker_root:/root/RTML -v /home/fidji/mdailey/COCO:/root/COCO -v /home/fidji/mdailey/Cityscapes:/root/Cityscapes -v /home/fidji/mdailey/LibriSpeech:/root/LibriSpeech --gpus all --name akraradet-lab akraradet-lab
+docker run -d -p 21413:22 -v $HOME/docker_root:/root/RTML -v /home/fidji/mdailey/COCO:/root/COCO -v /home/fidji/mdailey/Cityscapes:/root/Cityscapes -v /home/fidji/mdailey/LibriSpeech:/root/LibriSpeech --gpus all --name akraradet-lab akraradet-lab
