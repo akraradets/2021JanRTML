60b6dc5df13ca494b92c3b8c8738d89ae4cd8dd4
akrarads <akrarads@gmail.com>
2021-04-23 04:49:17 +0000

    duealing DQN

--- lab14/myDQN.py
+++ lab14/myDQN.py
@@ -7,6 +7,49 @@ import torch.nn.functional as F
 import numpy as np
 from collections import deque
 
+class DDQN(nn.Module):
+    def __init__(self, n_channel, n_action):
+        super(DDQN, self).__init__()        
+        
+        self.conv1 = nn.Conv2d(in_channels=n_channel, out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        
+        # advantage layer: output is n_action as usual
+        self.advantage = nn.Sequential(
+            nn.Linear(7*7*64, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_action)
+        )
+        
+        # policy value: value action
+        self.value = nn.Sequential(
+            nn.Linear(7*7*64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        x = x.view(x.size(0), -1)
+        advantage = self.advantage(x)
+        value     = self.value(x)
+
+        # recalculate Q-value
+        return value + advantage - advantage.mean()
+    
+    def act(self, state, epsilon, env, device):
+        if random.random() > epsilon:
+            state   = autograd.Variable(torch.FloatTensor(state).unsqueeze(0), volatile=True).to(device)
+            q_value = self.forward(state)
+            action  = q_value.max(1)[1].item()
+        else:
+            action = random.randrange(env.action_space.n)
+        return action
+
 
 class DQN(nn.Module):
     def __init__(self, n_state, n_action):
